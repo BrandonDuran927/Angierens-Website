288ee8983135ca44f9b19e87c1a781ac816e863b
n0zzzy <manzoncarlo345@gmail.com>
2026-01-17 23:24:46 +0800

    In Order Cart Section and Specific Order Section,  the Color of Refund button is red before, now changed it to Grey. It because for not attracting the customer refund button

--- src/routes/customer-interface/order.lazy.tsx
+++ src/routes/customer-interface/order.lazy.tsx
@@ -1261,7 +1261,7 @@ function RouteComponent() {
                                                                 e.stopPropagation();
                                                                 handleOpenRefundModal(order.id);
                                                             }}
-                                                            className="px-3 sm:px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 text-sm sm:text-base"
+                                                            className="px-3 sm:px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 text-sm sm:text-base"
                                                         >
                                                             Refund
                                                         </button>
